b6edc76ac44389562b8acd570006a135ac1154ae
whyal <tydevsforfun@gmail.com>
2023-07-14 20:47:40 +0800

    config changes

--- next.config.js
+++ next.config.js
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
-        port: "",
+        hostname: "**",
       },
     ],
   },
