436fe64d1a0de79543900a746917d1de57ce53f9
Whyal <tydevsforfun@gmail.com>
2023-07-27 00:14:10 +0800

    Added all source for portfolio videos

--- src/components/PortfolioModal.jsx
+++ src/components/PortfolioModal.jsx
@@ -1,9 +1,9 @@
-const Modal = ({isActive, onClose}) => {
+const Modal = ({isActive, onClose, src}) => {
     if (!isActive) return null
     return ( 
     <div className="w-screen h-screen fixed top-0 left-0 z-10 bg-[rgba(0,0,0,.5)] text-white flex justify-center items-center" >
         <p onClick={onClose} className="font-bold absolute top-1 right-1 bg-[#ff4343] px-3 py-4 rounded hover:opacity-80 cursor-pointer">Close</p>
-        <iframe className="w-11/12 aspect-video max-w-screen-2xl xl:w-3/4" src="//www.youtube.com/embed/D0UnqGm_miA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" allowFullScreen></iframe>
+        <iframe className="w-11/12 aspect-video max-w-screen-2xl xl:w-3/4" src={src} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;" allowFullScreen></iframe>
     </div> );
 }
  
